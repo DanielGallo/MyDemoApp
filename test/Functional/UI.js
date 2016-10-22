0bc4414b8ef40640316fb24781f6ddc37d284d43
Daniel Gallo <daniel.gallo@sencha.com>
2016-10-21 18:57:59 -0700

    Initial

--- test/Functional/UI.js
+++ test/Functional/UI.js
@@ -1,5 +1,8 @@
 describe("UI", function() {
     it("Looks nice", function() {
+        /*ST.grid('@mainlist')
+            .rowAt(1)
+            .click()*/
         ST.screenshot('Main Page');
     });
 });
